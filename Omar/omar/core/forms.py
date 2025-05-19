755ee72b25f464fc4b516ebbd979df54e7346e7d
omoqbel1 <164336396+omoqbel1@users.noreply.github.com>
2025-05-18 19:43:40 -0500

    Delete Omar/omar directory

--- Omar/omar/core/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import CompanyInfo
-
-class CompanyInfoForm(forms.ModelForm):
-    class Meta:
-        model = CompanyInfo
-        fields = ['company_name', 'logo', 'contact', 'mc_scac', 'email', 'address1', 'address2', 'city', 'state', 'zip_code', 'phone', 'fax']
